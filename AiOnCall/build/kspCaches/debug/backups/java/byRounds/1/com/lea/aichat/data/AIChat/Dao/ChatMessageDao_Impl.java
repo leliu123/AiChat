598605fd55c0a0ef0ebf561018935eccd66086cd
lea <2291104038@qq.com>
2025-12-02 12:54:26 +0800

    美化格式

---
+++ AiOnCall/build/kspCaches/debug/backups/java/byRounds/1/com/lea/aichat/data/AIChat/Dao/ChatMessageDao_Impl.java
@@ -0,0 +1,253 @@
+package com.lea.aichat.data.AIChat.Dao;
+
+import android.database.Cursor;
+import android.os.CancellationSignal;
+import androidx.annotation.NonNull;
+import androidx.room.CoroutinesRoom;
+import androidx.room.EntityDeletionOrUpdateAdapter;
+import androidx.room.EntityInsertionAdapter;
+import androidx.room.RoomDatabase;
+import androidx.room.RoomSQLiteQuery;
+import androidx.room.SharedSQLiteStatement;
+import androidx.room.util.CursorUtil;
+import androidx.room.util.DBUtil;
+import androidx.sqlite.db.SupportSQLiteStatement;
+import com.lea.aichat.data.AIChat.Entity.ChatMessageEntity;
+import java.lang.Class;
+import java.lang.Exception;
+import java.lang.Long;
+import java.lang.Object;
+import java.lang.Override;
+import java.lang.String;
+import java.lang.SuppressWarnings;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.concurrent.Callable;
+import javax.annotation.processing.Generated;
+import kotlin.Unit;
+import kotlin.coroutines.Continuation;
+import kotlinx.coroutines.flow.Flow;
+
+@Generated("androidx.room.RoomProcessor")
+@SuppressWarnings({"unchecked", "deprecation"})
+public final class ChatMessageDao_Impl implements ChatMessageDao {
+  private final RoomDatabase __db;
+
+  private final EntityInsertionAdapter<ChatMessageEntity> __insertionAdapterOfChatMessageEntity;
+
+  private final EntityDeletionOrUpdateAdapter<ChatMessageEntity> __deletionAdapterOfChatMessageEntity;
+
+  private final SharedSQLiteStatement __preparedStmtOfDeleteAllMessages;
+
+  public ChatMessageDao_Impl(@NonNull final RoomDatabase __db) {
+    this.__db = __db;
+    this.__insertionAdapterOfChatMessageEntity = new EntityInsertionAdapter<ChatMessageEntity>(__db) {
+      @Override
+      @NonNull
+      protected String createQuery() {
+        return "INSERT OR REPLACE INTO `chat_messages` (`id`,`text`,`isUser`,`timestamp`) VALUES (nullif(?, 0),?,?,?)";
+      }
+
+      @Override
+      protected void bind(@NonNull final SupportSQLiteStatement statement,
+          @NonNull final ChatMessageEntity entity) {
+        statement.bindLong(1, entity.getId());
+        statement.bindString(2, entity.getText());
+        final int _tmp = entity.isUser() ? 1 : 0;
+        statement.bindLong(3, _tmp);
+        statement.bindLong(4, entity.getTimestamp());
+      }
+    };
+    this.__deletionAdapterOfChatMessageEntity = new EntityDeletionOrUpdateAdapter<ChatMessageEntity>(__db) {
+      @Override
+      @NonNull
+      protected String createQuery() {
+        return "DELETE FROM `chat_messages` WHERE `id` = ?";
+      }
+
+      @Override
+      protected void bind(@NonNull final SupportSQLiteStatement statement,
+          @NonNull final ChatMessageEntity entity) {
+        statement.bindLong(1, entity.getId());
+      }
+    };
+    this.__preparedStmtOfDeleteAllMessages = new SharedSQLiteStatement(__db) {
+      @Override
+      @NonNull
+      public String createQuery() {
+        final String _query = "DELETE FROM chat_messages";
+        return _query;
+      }
+    };
+  }
+
+  @Override
+  public Object insertMessage(final ChatMessageEntity message,
+      final Continuation<? super Long> $completion) {
+    return CoroutinesRoom.execute(__db, true, new Callable<Long>() {
+      @Override
+      @NonNull
+      public Long call() throws Exception {
+        __db.beginTransaction();
+        try {
+          final Long _result = __insertionAdapterOfChatMessageEntity.insertAndReturnId(message);
+          __db.setTransactionSuccessful();
+          return _result;
+        } finally {
+          __db.endTransaction();
+        }
+      }
+    }, $completion);
+  }
+
+  @Override
+  public Object insertMessages(final List<ChatMessageEntity> messages,
+      final Continuation<? super Unit> $completion) {
+    return CoroutinesRoom.execute(__db, true, new Callable<Unit>() {
+      @Override
+      @NonNull
+      public Unit call() throws Exception {
+        __db.beginTransaction();
+        try {
+          __insertionAdapterOfChatMessageEntity.insert(messages);
+          __db.setTransactionSuccessful();
+          return Unit.INSTANCE;
+        } finally {
+          __db.endTransaction();
+        }
+      }
+    }, $completion);
+  }
+
+  @Override
+  public Object deleteMessage(final ChatMessageEntity message,
+      final Continuation<? super Unit> $completion) {
+    return CoroutinesRoom.execute(__db, true, new Callable<Unit>() {
+      @Override
+      @NonNull
+      public Unit call() throws Exception {
+        __db.beginTransaction();
+        try {
+          __deletionAdapterOfChatMessageEntity.handle(message);
+          __db.setTransactionSuccessful();
+          return Unit.INSTANCE;
+        } finally {
+          __db.endTransaction();
+        }
+      }
+    }, $completion);
+  }
+
+  @Override
+  public Object deleteAllMessages(final Continuation<? super Unit> $completion) {
+    return CoroutinesRoom.execute(__db, true, new Callable<Unit>() {
+      @Override
+      @NonNull
+      public Unit call() throws Exception {
+        final SupportSQLiteStatement _stmt = __preparedStmtOfDeleteAllMessages.acquire();
+        try {
+          __db.beginTransaction();
+          try {
+            _stmt.executeUpdateDelete();
+            __db.setTransactionSuccessful();
+            return Unit.INSTANCE;
+          } finally {
+            __db.endTransaction();
+          }
+        } finally {
+          __preparedStmtOfDeleteAllMessages.release(_stmt);
+        }
+      }
+    }, $completion);
+  }
+
+  @Override
+  public Flow<List<ChatMessageEntity>> getAllMessages() {
+    final String _sql = "SELECT * FROM chat_messages ORDER BY timestamp ASC";
+    final RoomSQLiteQuery _statement = RoomSQLiteQuery.acquire(_sql, 0);
+    return CoroutinesRoom.createFlow(__db, false, new String[] {"chat_messages"}, new Callable<List<ChatMessageEntity>>() {
+      @Override
+      @NonNull
+      public List<ChatMessageEntity> call() throws Exception {
+        final Cursor _cursor = DBUtil.query(__db, _statement, false, null);
+        try {
+          final int _cursorIndexOfId = CursorUtil.getColumnIndexOrThrow(_cursor, "id");
+          final int _cursorIndexOfText = CursorUtil.getColumnIndexOrThrow(_cursor, "text");
+          final int _cursorIndexOfIsUser = CursorUtil.getColumnIndexOrThrow(_cursor, "isUser");
+          final int _cursorIndexOfTimestamp = CursorUtil.getColumnIndexOrThrow(_cursor, "timestamp");
+          final List<ChatMessageEntity> _result = new ArrayList<ChatMessageEntity>(_cursor.getCount());
+          while (_cursor.moveToNext()) {
+            final ChatMessageEntity _item;
+            final long _tmpId;
+            _tmpId = _cursor.getLong(_cursorIndexOfId);
+            final String _tmpText;
+            _tmpText = _cursor.getString(_cursorIndexOfText);
+            final boolean _tmpIsUser;
+            final int _tmp;
+            _tmp = _cursor.getInt(_cursorIndexOfIsUser);
+            _tmpIsUser = _tmp != 0;
+            final long _tmpTimestamp;
+            _tmpTimestamp = _cursor.getLong(_cursorIndexOfTimestamp);
+            _item = new ChatMessageEntity(_tmpId,_tmpText,_tmpIsUser,_tmpTimestamp);
+            _result.add(_item);
+          }
+          return _result;
+        } finally {
+          _cursor.close();
+        }
+      }
+
+      @Override
+      protected void finalize() {
+        _statement.release();
+      }
+    });
+  }
+
+  @Override
+  public Object getAllMessagesSync(
+      final Continuation<? super List<ChatMessageEntity>> $completion) {
+    final String _sql = "SELECT * FROM chat_messages ORDER BY timestamp ASC";
+    final RoomSQLiteQuery _statement = RoomSQLiteQuery.acquire(_sql, 0);
+    final CancellationSignal _cancellationSignal = DBUtil.createCancellationSignal();
+    return CoroutinesRoom.execute(__db, false, _cancellationSignal, new Callable<List<ChatMessageEntity>>() {
+      @Override
+      @NonNull
+      public List<ChatMessageEntity> call() throws Exception {
+        final Cursor _cursor = DBUtil.query(__db, _statement, false, null);
+        try {
+          final int _cursorIndexOfId = CursorUtil.getColumnIndexOrThrow(_cursor, "id");
+          final int _cursorIndexOfText = CursorUtil.getColumnIndexOrThrow(_cursor, "text");
+          final int _cursorIndexOfIsUser = CursorUtil.getColumnIndexOrThrow(_cursor, "isUser");
+          final int _cursorIndexOfTimestamp = CursorUtil.getColumnIndexOrThrow(_cursor, "timestamp");
+          final List<ChatMessageEntity> _result = new ArrayList<ChatMessageEntity>(_cursor.getCount());
+          while (_cursor.moveToNext()) {
+            final ChatMessageEntity _item;
+            final long _tmpId;
+            _tmpId = _cursor.getLong(_cursorIndexOfId);
+            final String _tmpText;
+            _tmpText = _cursor.getString(_cursorIndexOfText);
+            final boolean _tmpIsUser;
+            final int _tmp;
+            _tmp = _cursor.getInt(_cursorIndexOfIsUser);
+            _tmpIsUser = _tmp != 0;
+            final long _tmpTimestamp;
+            _tmpTimestamp = _cursor.getLong(_cursorIndexOfTimestamp);
+            _item = new ChatMessageEntity(_tmpId,_tmpText,_tmpIsUser,_tmpTimestamp);
+            _result.add(_item);
+          }
+          return _result;
+        } finally {
+          _cursor.close();
+          _statement.release();
+        }
+      }
+    }, $completion);
+  }
+
+  @NonNull
+  public static List<Class<?>> getRequiredConverters() {
+    return Collections.emptyList();
+  }
+}
--- app/src/main/java/com/lea/aichat/ui/aiChat/aiChatScreen.kt
+++ app/src/main/java/com/lea/aichat/ui/aiChat/aiChatScreen.kt
@@ -1,6 +1,8 @@
 package com.lea.aichat.ui.aiChat
 
 import android.util.Log
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -11,42 +13,49 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.widthIn
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.lazy.rememberLazyListState
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.Send
 import androidx.compose.material.icons.filled.Delete
+import androidx.compose.material.icons.filled.Refresh
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
-import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.lea.aichat.ui.theme.AiChatTheme
-import com.lea.aichat.data.AIChat.chat.ChatMessage
-import com.lea.aichat.ui.aiChat.AIChatViewModelFactory
-import androidx.compose.ui.platform.LocalContext
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import com.lea.aichat.R
 import androidx.lifecycle.viewmodel.compose.viewModel
+import com.lea.aichat.R
+import com.lea.aichat.data.AIChat.chat.ChatMessage
+import com.lea.aichat.ui.aiChat.AIChatViewModelFactory
+import com.lea.aichat.ui.theme.AiChatTheme
+import dev.jeziellago.compose.markdowntext.MarkdownText
 
 @Composable
 fun AiChatScreen(
-    viewModel: AIChatViewModel= viewModel(factory = AIChatViewModelFactory(LocalContext.current)),
-
-                 ) {
+    viewModel: AIChatViewModel = viewModel(factory = AIChatViewModelFactory(LocalContext.current)),
+) {
     val uiState by viewModel.uiState.collectAsState()
-    val lazyListState= rememberLazyListState()
+    val lazyListState = rememberLazyListState()
 
     LaunchedEffect(uiState.chatMessages.size) {
         if (uiState.chatMessages.isNotEmpty()) {
@@ -54,142 +63,261 @@ fun AiChatScreen(
         }
     }
 
-    Column(
+    val backgroundBrush = Brush.verticalGradient(
+        colors = listOf(
+            MaterialTheme.colorScheme.surface,
+            MaterialTheme.colorScheme.surfaceVariant,
+            MaterialTheme.colorScheme.surface
+        )
+    )
+
+    Box(
         modifier = Modifier
             .fillMaxSize()
+            .background(backgroundBrush)
             .padding(16.dp)
-    ){//标题栏
-        Row(
-            modifier = Modifier
-                .fillMaxWidth()
-                .align(Alignment.CenterHorizontally) ,
-            horizontalArrangement = Arrangement.SpaceBetween,
-            verticalAlignment = Alignment.CenterVertically
-
-        ){
-            Text(
-                text = stringResource(R.string.aiName),
-                style = MaterialTheme.typography.headlineSmall,
-                //正中间
-
-
+    ) {
+        Column(
+            modifier = Modifier.fillMaxSize(),
+            verticalArrangement = Arrangement.spacedBy(12.dp)
+        ) {
+            ChatHeader(
+                title = stringResource(R.string.aiName),
+                subTitle = if (uiState.isLoading) "正在思考..." else "和 AI 随时畅聊",
+                onClear = { viewModel.processIntent(ChatIntent.ClearChat) },
+                enabled = uiState.chatMessages.isNotEmpty() && !uiState.isLoading
             )
-            IconButton(
-                onClick = {
-                    viewModel.processIntent(ChatIntent.ClearChat)
+
+            Card(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .weight(1f),
+                colors = CardDefaults.cardColors(
+                    containerColor = MaterialTheme.colorScheme.surface.copy(alpha = 0.95f)
+                ),
+                elevation = CardDefaults.cardElevation(defaultElevation = 3.dp)
+            ) {
+                if (uiState.chatMessages.isEmpty()) {
+                    EmptyState()
+                } else {
+                    LazyColumn(
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .padding(12.dp),
+                        state = lazyListState,
+                        verticalArrangement = Arrangement.spacedBy(12.dp)
+                    ) {
+                        items(uiState.chatMessages) { chatMessage ->
+                            ChatMessageItem(chatMessage = chatMessage)
+                        }
+                    }
+                }
+            }
+
+            if (uiState.isLoading) {
+                LoadingIndicatorBar()
+            }
+
+            ChatInputBar(
+                text = uiState.inputText,
+                onTextChange = {
+                    Log.d("AiChatScreen", "InputTextChange: $it")
+                    viewModel.processIntent(ChatIntent.InputTextChange(it))
                 },
-                enabled =uiState.chatMessages.isNotEmpty()&&!uiState.isLoading
-            ) {
-                Icon(Icons.Default.Delete, contentDescription = stringResource(R.string.clearChat))
-
-            }
+                onSend = { viewModel.processIntent(ChatIntent.SendMessage) },
+                enabled = uiState.inputText.isNotEmpty() && !uiState.isLoading,
+                onClearDraft = { viewModel.processIntent(ChatIntent.InputTextChange("")) }
+            )
         }
-        Spacer(modifier = Modifier.height(16.dp))
-
-            // 聊天消息列表
-        LazyColumn(
-                modifier = Modifier.fillMaxWidth().weight(1f),
-                state = lazyListState,
-                verticalArrangement = Arrangement.spacedBy(8.dp)
-            ) {
-                items(uiState.chatMessages) {chatMessage->
-                    ChatMessageItem(chatMessage = chatMessage)
-
-                }
-
-
-            }
-
-        Spacer(modifier = Modifier.height(16.dp))
-
-        Row(modifier = Modifier,
-                verticalAlignment = Alignment.CenterVertically
-            ){
-                TextField(
-                    value = uiState.inputText,
-                    onValueChange = {
-
-                        text->
-                        Log.d("AiChatScreen", "InputTextChange: $text")
-                        viewModel.processIntent(ChatIntent.InputTextChange(text))
-                        // 打印输入的文本
-                    },
-                    modifier = Modifier.weight(1f),
-                    placeholder = { Text("输入消息...") },
-                    singleLine = false,
-                    maxLines = 3,
-
-
-
-                )
-
-                Spacer(modifier = Modifier.width(8.dp))
-                IconButton(
-                    onClick = {
-                        viewModel.processIntent(ChatIntent.SendMessage)
-                    },
-                    enabled = uiState.inputText.isNotEmpty()&&!uiState.isLoading
-                ) {
-                    Icon(Icons.AutoMirrored.Filled.Send, contentDescription ="发送")
-
-                }
-            }
-
-        }
+    }
 }
-
-
-
-
-
-
 
 @Composable
 fun ChatMessageItem(chatMessage: ChatMessage) {
     val alignment = if (chatMessage.isUser) Alignment.CenterEnd else Alignment.CenterStart
-    val containerColor = if (chatMessage.isUser) {
+    val bubbleColor = if (chatMessage.isUser) {
         MaterialTheme.colorScheme.primaryContainer
     } else {
-        MaterialTheme.colorScheme.surfaceVariant
+        MaterialTheme.colorScheme.secondaryContainer
     }
+    val onBubbleColor = if (chatMessage.isUser) {
+        MaterialTheme.colorScheme.onPrimaryContainer
+    } else {
+        MaterialTheme.colorScheme.onSecondaryContainer
+    }
+
     Box(
         modifier = Modifier.fillMaxWidth(),
         contentAlignment = alignment
     ) {
-        Card(
-            colors = CardDefaults.cardColors(containerColor = containerColor)
-        ) {
-            Row(
-                verticalAlignment = Alignment.CenterVertically
-            ) {
-                Column(
-                    modifier = Modifier.padding(12.dp).weight(1f)
-                ) {
-                    Text(
-                        text = if (chatMessage.isUser) "你" else "AI",
-                        style = MaterialTheme.typography.labelSmall,
-                        color = MaterialTheme.colorScheme.onSurfaceVariant
+        Column(
+            modifier = Modifier
+                .clip(
+                    RoundedCornerShape(
+                        topStart = 20.dp,
+                        topEnd = 20.dp,
+                        bottomEnd = if (chatMessage.isUser) 2.dp else 20.dp,
+                        bottomStart = if (chatMessage.isUser) 20.dp else 2.dp
                     )
-                    Spacer(modifier = Modifier.height(4.dp))
+                )
+                .background(bubbleColor)
+                .border(
+                    width = 1.dp,
+                    color = bubbleColor.copy(alpha = 0.4f),
+                    shape = RoundedCornerShape(20.dp)
+                )
+                .padding(horizontal = 14.dp, vertical = 10.dp)
+                .widthIn(max = 320.dp)
+        ) {
+            Text(
+                text = if (chatMessage.isUser) "你" else "AI",
+                style = MaterialTheme.typography.labelSmall,
+                color = onBubbleColor.copy(alpha = 0.8f),
+                fontWeight = FontWeight.SemiBold
+            )
+            Spacer(modifier = Modifier.height(6.dp))
 
-                    Text(
-                            text = chatMessage.text,
-                            style = MaterialTheme.typography.bodyMedium
-                        )
-
-                }
-
-
-
+            if (chatMessage.isUser) {
+                Text(
+                    text = chatMessage.text,
+                    style = MaterialTheme.typography.bodyMedium,
+                    color = onBubbleColor
+                )
+            } else {
+                MarkdownText(
+                    modifier = Modifier.fillMaxWidth(),
+                    markdown = chatMessage.text,
+                    style = MaterialTheme.typography.bodyMedium.copy(color = onBubbleColor)
+                )
             }
         }
     }
 }
 
+@Composable
+private fun ChatHeader(
+    title: String,
+    subTitle: String,
+    onClear: () -> Unit,
+    enabled: Boolean
+) {
+    Row(
+        modifier = Modifier
+            .fillMaxWidth()
+            .clip(RoundedCornerShape(24.dp))
+            .background(MaterialTheme.colorScheme.surfaceContainerHighest.copy(alpha = 0.9f))
+            .padding(horizontal = 18.dp, vertical = 12.dp),
+        horizontalArrangement = Arrangement.SpaceBetween,
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        Column {
+            Text(
+                text = title,
+                style = MaterialTheme.typography.headlineSmall,
+                fontWeight = FontWeight.Bold
+            )
+            Spacer(modifier = Modifier.height(4.dp))
+            Text(
+                text = subTitle,
+                style = MaterialTheme.typography.bodySmall,
+                color = MaterialTheme.colorScheme.onSurfaceVariant
+            )
+        }
+        IconButton(onClick = onClear, enabled = enabled) {
+            Icon(Icons.Default.Delete, contentDescription = stringResource(R.string.clearChat))
+        }
+    }
+}
 
+@Composable
+private fun ChatInputBar(
+    text: String,
+    onTextChange: (String) -> Unit,
+    onSend: () -> Unit,
+    enabled: Boolean,
+    onClearDraft: () -> Unit
+) {
+    Row(
+        modifier = Modifier
+            .fillMaxWidth()
+            .clip(RoundedCornerShape(28.dp))
+            .background(MaterialTheme.colorScheme.surface.copy(alpha = 0.95f))
+            .border(
+                width = 1.dp,
+                color = MaterialTheme.colorScheme.outline.copy(alpha = 0.4f),
+                shape = RoundedCornerShape(28.dp)
+            )
+            .padding(horizontal = 12.dp, vertical = 8.dp),
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        OutlinedTextField(
+            value = text,
+            onValueChange = onTextChange,
+            modifier = Modifier.weight(1f),
+            placeholder = { Text("输入消息...") },
+            singleLine = false,
+            maxLines = 4,
+            shape = RoundedCornerShape(18.dp)
+        )
+        Spacer(modifier = Modifier.width(8.dp))
+        IconButton(onClick = onClearDraft, enabled = text.isNotEmpty()) {
+            Icon(Icons.Default.Refresh, contentDescription = "清空输入")
+        }
+        IconButton(
+            onClick = onSend,
+            enabled = enabled
+        ) {
+            Icon(Icons.AutoMirrored.Filled.Send, contentDescription = "发送")
+        }
+    }
+}
 
+@Composable
+private fun LoadingIndicatorBar() {
+    Row(
+        modifier = Modifier
+            .fillMaxWidth()
+            .clip(RoundedCornerShape(16.dp))
+            .background(MaterialTheme.colorScheme.surfaceVariant.copy(alpha = 0.7f))
+            .padding(horizontal = 14.dp, vertical = 8.dp),
+        verticalAlignment = Alignment.CenterVertically,
+        horizontalArrangement = Arrangement.spacedBy(12.dp)
+    ) {
+        CircularProgressIndicator(
+            modifier = Modifier
+                .width(20.dp)
+                .height(20.dp),
+            strokeWidth = 2.dp
+        )
+        Text(
+            text = "AI 正在回复...",
+            style = MaterialTheme.typography.bodySmall,
+            color = MaterialTheme.colorScheme.onSurfaceVariant
+        )
+    }
+}
 
-
+@Composable
+private fun EmptyState() {
+    Column(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(vertical = 40.dp),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.spacedBy(12.dp)
+    ) {
+        Text(
+            text = "开始一段新的对话",
+            style = MaterialTheme.typography.titleMedium,
+            fontWeight = FontWeight.SemiBold
+        )
+        Text(
+            text = "发送你的第一个问题吧，我已经准备好啦。",
+            style = MaterialTheme.typography.bodyMedium,
+            color = MaterialTheme.colorScheme.onSurfaceVariant
+        )
+    }
+}
 
 @Preview(showBackground = true)
 @Composable
@@ -198,3 +326,4 @@ fun AiChatScreenPreview() {
         AiChatScreen()
     }
 }
+
